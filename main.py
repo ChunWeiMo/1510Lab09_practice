605350877f0f68551d7ba1d53fedf7d3c89cf99b
Chun-Wei, Mo <chunwei.mo@gmail.com>
2023-11-22 19:26:58 -0800

    Create check_input_positive

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def get_words_list(url):
     except requests.exceptions.MissingSchema:
         print("Enter a correct URL")
         return
-    
+
     whole_book = response.text
     whole_book = whole_book.lower().rstrip().lstrip()
     marks = [',', '.', '?', '!', '(', ')', '-',
@@ -18,6 +18,8 @@ def get_words_list(url):
     words = whole_book.split()
     return words
 
+def check_input_positive(input_integer):
+    pass
 
 def get_value(word):
     return word[1]
@@ -36,34 +38,34 @@ def sorted_by_word_count(words_list):
 
 
 def ask_place(sorted_words_list):
-    place = input('Enter a positive integer: ')    
+    place = input('Enter a positive integer: ')
     try:
         place = int(place)
     except ValueError:
         print("Please enter an integer")
         return
-    
+
     print(f'The {place}th most frequent word in this file is:')
     try:
-        print(f'{sorted_words_list[place-1][0]} {sorted_words_list[place-1][1]}')
+        print(
+            f'{sorted_words_list[place-1][0]} {sorted_words_list[place-1][1]}')
     except IndexError:
         print("The input number is out of list length")
-        
-
 
 
 def main():
-    url = "https://www.gutenberg.org/cache/epub/72013/pg72013.txt"
-    # url='abc'
+    url = "https://www.gutenberg.org/cache/epub/72013/pg72013.txt" # the book I pick
+    url = "https://www.gutenberg.org/cache/epub/2554/pg2554.txt"  # Lab09 file
+    # url = 'https://www.google.ca'
+    # url = 'abc'
     words_list = get_words_list(url)
-    sorted_words_list = sorted_by_word_count(words_list)
-
-    for order in range(10):
-        print(
-            f'{order+1}. {sorted_words_list[order][0]} {sorted_words_list[order][1]:,}')
-    ask_place(sorted_words_list)
-    
-    
+    if words_list:
+        sorted_words_list = sorted_by_word_count(words_list)
+        for order in range(10):
+            print(
+                f'{order+1}. {sorted_words_list[order][0]} {sorted_words_list[order][1]:,}')
+        ask_place(sorted_words_list)
+    print("\nPROGRAM END.")
 
 
 if __name__ == '__main__':
