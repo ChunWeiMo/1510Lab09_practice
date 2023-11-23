da27c4952a263964466d3729ff89478f4f608649
Chun-Wei, Mo <chunwei.mo@gmail.com>
2023-11-22 20:44:54 -0800

    Replace marks by isalnum()

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ def get_words_list(url):
     except requests.exceptions.MissingSchema:
         print("Enter a correct URL")
         return
-
     whole_book = response.text
     whole_book = whole_book.lower().rstrip().lstrip()
-    marks = [',', '.', '?', '!', '(', ')', '-',
-             '――', ':', '“', '”', '"', ';', '‘', ]
-    for mark in marks:
-        whole_book = whole_book.replace(mark, ' ')
+    
+    whole_book_no_punctuation = ''
+    for character in whole_book:
+        if character.isalnum() == False:
+            character = ' '
+        whole_book_no_punctuation += character   
 
-    words = whole_book.split()
+    words = whole_book_no_punctuation.split()
     return words
 
 
@@ -62,10 +63,8 @@ def print_input_order(order, sorted_words_list):
 
 
 def main():
-    # url = "https://www.gutenberg.org/cache/epub/72013/pg72013.txt"  # the book I pick
-    url = "https://www.gutenberg.org/cache/epub/2554/pg2554.txt"  # Lab09 file
-    # url = 'https://www.google.ca'
-    # url = 'abc'
+    # url = "https://www.gutenberg.org/cache/epub/2554/pg2554.txt"  # Lab09 file
+    url = "https://www.gutenberg.org/cache/epub/72013/pg72013.txt"  # the book I pick
     words_list = get_words_list(url)
     if words_list:
         sorted_words_list = sorted_by_word_count(words_list)
@@ -82,7 +81,7 @@ def main():
             print_input_order(order, sorted_words_list)
         finally:
             print("\nPROGRAM END.")
-
+    
 
 if __name__ == '__main__':
     main()
